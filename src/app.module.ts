eb6eceacefd82c8c459ab252560175ee33b4a874
Yana Malakhova <ya6ka0@gmail.com>
2025-11-29 19:42:46 +0300

    chore: remove default nestjs files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
+  controllers: [],
+  providers: [],
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
