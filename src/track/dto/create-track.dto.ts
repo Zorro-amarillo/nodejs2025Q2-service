086de4c3613382cd8ec571d8a11e5e1b8e1ef225
Yana Malakhova <ya6ka0@gmail.com>
2025-12-02 12:24:58 +0300

    feat: mark artist/album IDs as nullavle in OpenAPI schema

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -8,11 +8,21 @@ export class CreateTrackDto {
   name: string;
 
   @ValidateIf((obj) => typeof obj.artistId !== undefined)
-  @ApiProperty({ example: '8e611ac7-420a-42a9-8240-c49b91f93184' })
+  @ApiProperty({
+    nullable: true,
+    type: 'string',
+    format: 'uuid',
+    example: '8e611ac7-420a-42a9-8240-c49b91f93184',
+  })
   artistId: string | null;
 
   @ValidateIf((obj) => typeof obj.albumId !== undefined)
-  @ApiProperty({ example: '8e611ac7-420a-42a9-8240-c49b91f93183' })
+  @ApiProperty({
+    nullable: true,
+    type: 'string',
+    format: 'uuid',
+    example: '8e611ac7-420a-42a9-8240-c49b91f93183',
+  })
   albumId: string | null;
 
   @IsNotEmpty()
